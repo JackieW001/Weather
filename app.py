76408e500081f9c25145441c8686de699607ef2c
Xavier Chen <xchen5@stuy.edu>
2017-11-19 21:07:01 -0500

    Finished base64 encoding and implemented with Google Vision API

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
-#import request
-
+import requests, base64
 app = Flask(__name__)
+
+newJSON = ""
+
 
 @app.route("/")
 def root():
@@ -11,20 +13,34 @@ def root():
     response = requests.post(url='https://vision.googleapis.com/v1/images:annotate?key=%s'%(key), data=data, headers={'Content-Type': 'application/json'})
     print response.text
     '''
+    key = open('apiKey.txt', 'rb').read()
+    data = replaceRequest("static/bigmac.jpg")
+    #print data
+    response = requests.post(url='https://vision.googleapis.com/v1/images:annotate?key=%s'%(key), data=data, headers={'Content-Type': 'application/json'})
+    print response.text
+
     return render_template("home.html", title="Weather")
 
 def getfile():
     return ""
 
-def baseEncode64():
-    return ""
+def baseEncode64(imagePath):
+    with open(imagePath, "rb") as image_file:
+        encode = base64.b64encode(image_file.read())
+        return encode
+
+def replaceRequest(imagePath):
+    encodeString = baseEncode64(imagePath)
+    data = open('request.json', 'rb').read()
+    newJSON = data.replace("toBeReplaced", encodeString)
+    return newJSON
 
 def callgvision():
     return ""
 
 def readgvision():
     return ""
-    
+
 @app.route("/recipes")
 def recipes():
     return render_template("recipes.html")
